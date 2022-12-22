07407bffa3ad339a04e351e74d6f793e679b6128
SamuelSill <mulisilman@gmail.com>
2022-12-23 01:28:42 +0200

    Reverted to non-dynamic version

--- src/s_interpreter/__init__.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-
-tag: str = subprocess.check_output(['git', 'describe', '--abbrev=0', '--tags']).decode("ascii").strip()
-__version__ = f"{tag}.{subprocess.check_output(['git', 'rev-list', tag + '..HEAD', '--count']).decode('ascii').strip()}"
